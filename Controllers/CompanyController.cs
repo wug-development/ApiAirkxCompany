a7e82a7a733716087bd9190b9c89de7ff1703459
wuguang <guangwu@roseonly.com>
2020-03-26 16:45:56 +0800

    解决VUE和Jquery(Callback)共用一个接口的兼容问题

--- Controllers/CompanyController.cs
+++ Controllers/CompanyController.cs
@@ -7,7 +7,9 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
+using System.Web.Helpers;
 using System.Web.Http;
+using System.Web.Http.Results;
 
 namespace ApiAirkxCompany.Controllers
 {
--- Global.asax.cs
+++ Global.asax.cs
@@ -1,14 +1,10 @@
 ﻿using Newtonsoft.Json.Converters;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
-using System.Web.Optimization;
-using System.Web.Routing;
 using WebApiContrib.Formatting.Jsonp;
-using log4net.Config;
+using System.Net.Http;
+using System.Web.Http.Filters;
+using System.Text;
 
 namespace ApiAirkxCompany
 {
@@ -16,21 +12,43 @@ namespace ApiAirkxCompany
     {
         protected void Application_Start()
         {
-            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.Converters.Insert(0, new JsonDataTimeConverter());
             AreaRegistration.RegisterAllAreas();
+            var config = GlobalConfiguration.Configuration;
+            GlobalConfiguration.Configuration.AddJsonpFormatter(config.Formatters.JsonForma‌​tter, "callback");
             GlobalConfiguration.Configure(WebApiConfig.Register);
             GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.Converters.Add(new IsoDateTimeConverter()
             {
                 DateTimeFormat = "yyyy-MM-dd HH:mm:ss",
             });
-            //GlobalConfiguration.Configuration.AddJsonpFormatter(GlobalConfiguration.Configuration.Formatters.JsonForma‌​tter, "callback");
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            //GlobalConfiguration.Configuration.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
 
             //应用程序启动时，自动加载配置log4Net
             log4net.Config.XmlConfigurator.Configure();
             GlobalConfiguration.Configuration.Filters.Add(new ApiTrackerFilter());
+            GlobalConfiguration.Configuration.Filters.Add(new JsonCallbackAttribute());
         }
     }
+
+    public class JsonCallbackAttribute : System.Web.Http.Filters.ActionFilterAttribute
+    {
+        private const string CallbackQueryParameter = "callback";
+
+        public override void OnActionExecuted(HttpActionExecutedContext context)
+        {
+            var callback = string.Empty;
+            if (IsJsonp(out callback))
+            {
+                var jsonBuilder = new StringBuilder(callback);
+                jsonBuilder.AppendFormat("({0})", context.Response.Content.ReadAsStringAsync().Result);
+                context.Response.Content = new StringContent(jsonBuilder.ToString());
+            }
+            base.OnActionExecuted(context);
+        }
+
+        private bool IsJsonp(out string callback)
+        {
+            callback = System.Web.HttpContext.Current.Request.QueryString[CallbackQueryParameter];
+            return !string.IsNullOrEmpty(callback);
+        }
+    }
+
 }
